774e6d3623f18030fd3feda92e21cc98a892a44d
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-16 20:06:16 +0530

    implement dynamic name setting...

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+   const [name, setName] = useState("unknown entity");
+   const inputRef = useRef();
+
+   const nameHandler = () => {
+      setName(inputRef.current.value);
+   };
+
+   return (
+      <section id="player">
+         <h2>Welcome {name}</h2>
+         <p>
+            <input ref={inputRef} type="text" />
+            <button onClick={nameHandler}>Set Name</button>
+         </p>
+      </section>
+   );
 }
